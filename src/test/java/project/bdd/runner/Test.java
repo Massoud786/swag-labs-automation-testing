d622043474765a444fef959f0f3fc101ff7bd628
Massoud786 <massoud152.homayoun@gmail.com>
2025-10-03 11:43:01 -0700

    add Cucumber HTML report plugin

--- src/test/java/project/bdd/runner/Test.java
+++ src/test/java/project/bdd/runner/Test.java
@@ -5,10 +5,16 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "classpath:features/InventoryTest.feature",
+        features = "classpath:features/LoginTest.feature",
         glue = "project.bdd.steps",
-        tags = "@inventory",
-        dryRun = false
+        tags = "@login",
+        dryRun = false,
+        plugin = {
+                "pretty",
+                "html:target/cucumber-html-report.html",
+                "json:target/cucumber.json",
+                "junit:target/cucumber.xml"
+        }
 )
 public class Test {
 
